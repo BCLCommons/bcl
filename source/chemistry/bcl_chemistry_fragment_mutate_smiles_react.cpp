69cafa63af5a072814d6b782caafd8832f53cfc7
brownbp1 <benjamin.p.brown17@gmail.com>
2022-12-07 09:04:19 -0600

    initial files for new smiles reaction class

---
+++ source/chemistry/bcl_chemistry_fragment_mutate_smiles_react.cpp
@@ -0,0 +1,326 @@
+// (c) Copyright BCL @ Vanderbilt University 2014
+// (c) BCL Homepage: http://www.meilerlab.org/bclcommons
+// (c) BCL Code Repository: https://github.com/BCLCommons/bcl
+// (c)
+// (c) The BioChemical Library (BCL) was originally developed by contributing members of the Meiler Lab @ Vanderbilt University.
+// (c)
+// (c) The BCL is now made available as an open-source software package distributed under the permissive MIT license,
+// (c) developed and maintained by the Meiler Lab at Vanderbilt University and contributing members of the BCL Commons.
+// (c)
+// (c) External code contributions to the BCL are welcome. Please visit the BCL Commons GitHub page for information on how you can contribute.
+// (c)
+// (c) This file is part of the BCL software suite and is made available under the MIT license.
+// (c)
+
+// initialize the static initialization fiasco finder, if macro ENABLE_FIASCO_FINDER is defined
+#include "util/bcl_util_static_initialization_fiasco_finder.h"
+BCL_StaticInitializationFiascoFinder
+
+// include header of this class
+#include "chemistry/bcl_chemistry_fragment_mutate_smiles_react.h"
+
+// includes from bcl - sorted alphabetically
+#include "chemistry/bcl_chemistry_atoms_complete_standardizer.h"
+#include "chemistry/bcl_chemistry_fragment_map_conformer.h"
+#include "chemistry/bcl_chemistry_fragment_track_mutable_atoms.h"
+#include "chemistry/bcl_chemistry_hydrogens_handler.h"
+#include "chemistry/bcl_chemistry_merge_fragment_complete.h"
+#include "chemistry/bcl_chemistry_rotamer_library_file.h"
+#include "chemistry/bcl_chemistry_voxel_grid_atom.h"
+#include "command/bcl_command_command_state.h"
+#include "descriptor/bcl_descriptor_cheminfo_properties.h"
+#include "find/bcl_find_collector_interface.h"
+#include "io/bcl_io_file.h"
+#include "io/bcl_io_ifstream.h"
+#include "random/bcl_random_uniform_distribution.h"
+#include "util/bcl_util_string_functions.h"
+// external includes - sorted alphabetically
+
+namespace bcl
+{
+  namespace chemistry
+  {
+
+  //////////
+  // data //
+  //////////
+
+    // add the interface to the set of known implementations
+    const util::SiPtr< const util::ObjectInterface> FragmentMutateSmilesReact::s_Instance
+    (
+      util::Enumerated< FragmentMutateInterface>::AddInstance( new FragmentMutateSmilesReact())
+    );
+
+  //////////////////////////////////
+  // construction and destruction //
+  //////////////////////////////////
+
+    //! @brief default constructor
+    FragmentMutateSmilesReact::FragmentMutateSmilesReact() :
+      m_MedChemFilename( std::string())
+    {
+      this->ReadInitializerSuccessHook( util::ObjectDataLabel(), util::GetLogger());
+    }
+
+    //! @brief construct with a pool of external fragments for fragment grow
+    //! @param FRAGMENT_POOL external fragments to add to base fragment
+    FragmentMutateSmilesReact::FragmentMutateSmilesReact
+    (
+      const bool &CORINA_CONFS
+    ) :
+      m_MedChemFilename( std::string())
+    {
+      m_Corina = CORINA_CONFS;
+
+      this->ReadInitializerSuccessHook( util::ObjectDataLabel(), util::GetLogger());
+    }
+
+    //! @brief druglikeness constructor
+    //! @param FRAGMENT_POOL external fragments to add to base fragment
+    //! @param DRUG_LIKENESS_TYPE type of druglikeness filter to apply during clean
+    FragmentMutateSmilesReact::FragmentMutateSmilesReact
+    (
+      const std::string &DRUG_LIKENESS_TYPE,
+      const bool &CORINA_CONFS
+    ) :
+      m_MedChemFilename( std::string())
+    {
+      m_DrugLikenessType = DRUG_LIKENESS_TYPE;
+      m_Corina = CORINA_CONFS;
+
+      this->ReadInitializerSuccessHook( util::ObjectDataLabel(), util::GetLogger());
+    }
+
+    //! @brief local mutate constructor
+    //! @param FRAGMENT_POOL external fragments to add to base fragment
+    //! @param DRUG_LIKENESS_TYPE type of druglikeness filter to apply during clean
+    //! @param SCAFFOLD_FRAGMENT fragment to which the new mutated molecule will be aligned based on substructure
+    //! @param MUTABLE_FRAGMENTS non-mutable component of the current molecule
+    //! @param MUTABLE_ATOM_INDICES indices of atoms that can be mutated
+    FragmentMutateSmilesReact::FragmentMutateSmilesReact
+    (
+      const std::string &DRUG_LIKENESS_TYPE,
+      const FragmentComplete &SCAFFOLD_FRAGMENT,
+      const FragmentEnsemble &MUTABLE_FRAGMENTS,
+      const storage::Vector< size_t> &MUTABLE_ATOM_INDICES,
+      const bool &CORINA_CONFS
+    ) :
+      m_MedChemFilename( std::string())
+    {
+      m_DrugLikenessType = DRUG_LIKENESS_TYPE;
+      m_ScaffoldFragment = SCAFFOLD_FRAGMENT;
+      m_MutableFragments = MUTABLE_FRAGMENTS;
+      m_MutableAtomIndices = MUTABLE_ATOM_INDICES;
+      m_Corina = CORINA_CONFS;
+
+      this->ReadInitializerSuccessHook( util::ObjectDataLabel(), util::GetLogger());
+    }
+
+    //! @brief local mutate pose-sensitive constructor
+    //! @param DRUG_LIKENESS_TYPE type of druglikeness filter to apply during clean
+    //! @param SCAFFOLD_FRAGMENT fragment to which the new mutated molecule will be aligned based on substructure
+    //! @param MUTABLE_FRAGMENTS non-mutable component of the current molecule
+    //! @param MUTABLE_ATOM_INDICES indices of atoms that can be mutated
+    //! @param MDL property label containing path to protein binding pocket PDB file
+    //! @param PROPERTY_SCORER property that will be used to score interactions with protein pocket
+    //! @param RESOLVE_CLASHES if true, resolve clashes with specified protein pocket after mutatation
+    //! @param BFACTORS vector of values indicating per-residue flexibility (higher values are more flexible)
+    FragmentMutateSmilesReact::FragmentMutateSmilesReact
+    (
+      const std::string &DRUG_LIKENESS_TYPE,
+      const FragmentComplete &SCAFFOLD_FRAGMENT,
+      const FragmentEnsemble &MUTABLE_FRAGMENTS,
+      const storage::Vector< size_t> &MUTABLE_ATOM_INDICES,
+      const std::string &MDL,
+      const descriptor::CheminfoProperty &PROPERTY_SCORER,
+      const bool &RESOLVE_CLASHES,
+      const storage::Vector< float> &BFACTORS,
+      const bool &CORINA_CONFS
+    ) :
+      m_MedChemFilename( std::string())
+    {
+      m_DrugLikenessType = DRUG_LIKENESS_TYPE;
+      m_ScaffoldFragment = SCAFFOLD_FRAGMENT;
+      m_MutableFragments = MUTABLE_FRAGMENTS;
+      m_MutableAtomIndices = MUTABLE_ATOM_INDICES;
+      m_MDL = MDL;
+      m_PropertyScorer = PROPERTY_SCORER;
+      m_ResolveClashes = RESOLVE_CLASHES;
+      m_BFactors = BFACTORS;
+      m_Corina = CORINA_CONFS;
+
+      this->ReadInitializerSuccessHook( util::ObjectDataLabel(), util::GetLogger());
+    }
+
+    //! @brief local mutate pose-sensitive constructor
+    //! @param DRUG_LIKENESS_TYPE type of druglikeness filter to apply during clean
+    //! @param SCAFFOLD_FRAGMENT fragment to which the new mutated molecule will be aligned based on substructure
+    //! @param MUTABLE_FRAGMENTS non-mutable component of the current molecule
+    //! @param MUTABLE_ATOM_INDICES indices of atoms that can be mutated
+    //! @param MDL property label containing path to protein binding pocket PDB file
+    //! @param PROPERTY_SCORER property that will be used to score interactions with protein pocket
+    //! @param RESOLVE_CLASHES if true, resolve clashes with specified protein pocket after mutatation
+    //! @param BFACTORS vector of values indicating per-residue flexibility (higher values are more flexible)
+    FragmentMutateSmilesReact::FragmentMutateSmilesReact
+    (
+      const std::string &DRUG_LIKENESS_TYPE,
+      const FragmentComplete &SCAFFOLD_FRAGMENT,
+      const FragmentEnsemble &MUTABLE_FRAGMENTS,
+      const storage::Vector< size_t> &MUTABLE_ATOM_INDICES,
+      const std::string &MDL,
+      const bool &RESOLVE_CLASHES,
+      const storage::Vector< float> &BFACTORS,
+      const bool &CORINA_CONFS
+    ) :
+      m_MedChemFilename( std::string())
+    {
+      m_DrugLikenessType = DRUG_LIKENESS_TYPE;
+      m_ScaffoldFragment = SCAFFOLD_FRAGMENT;
+      m_MutableFragments = MUTABLE_FRAGMENTS;
+      m_MutableAtomIndices = MUTABLE_ATOM_INDICES;
+      m_MDL = MDL;
+      m_ResolveClashes = RESOLVE_CLASHES;
+      m_BFactors = BFACTORS;
+      m_Corina = CORINA_CONFS;
+
+      this->ReadInitializerSuccessHook( util::ObjectDataLabel(), util::GetLogger());
+    }
+
+    //! @brief clone constructor
+    FragmentMutateSmilesReact *FragmentMutateSmilesReact::Clone() const
+    {
+      return new FragmentMutateSmilesReact( *this);
+    }
+
+  /////////////////
+  // data access //
+  /////////////////
+
+    //! @brief returns class name
+    //! @return the class name as const ref std::string
+    const std::string &FragmentMutateSmilesReact::GetClassIdentifier() const
+    {
+      return GetStaticClassName( *this);
+    }
+
+    //! @brief get a short name for this class
+    //! @return a short name for this class
+    const std::string &FragmentMutateSmilesReact::GetAlias() const
+    {
+      static const std::string s_name( "SmilesReact");
+      return s_name;
+    }
+
+  ///////////////
+  // operators //
+  ///////////////
+
+    //! @brief virtual operator taking an fragment and generating a new fragment by growing on a valence
+    //! @param FRAGMENT small molecule of interest
+    //! @return MutateResult with Constitution after the mutate
+    math::MutateResult< FragmentComplete> FragmentMutateSmilesReact::operator()( const FragmentComplete &FRAGMENT) const
+    {
+
+
+    }
+
+  ////////////////
+  // operations //
+  ////////////////
+
+    //! @brief set medchem fragment library from filename
+    void FragmentMutateSmilesReact::SetFragmentLibraryFromFilename( const std::string &FRAGMENTS_FILENAME)
+    {
+      s_Mutex.Lock();
+      io::IFStream input;
+      io::File::MustOpenIFStream( input, FRAGMENTS_FILENAME);
+
+      // TODO
+      // do something
+
+      io::File::CloseClearFStream( input);
+      s_Mutex.Unlock();
+    }
+
+  //////////////////////
+  // helper functions //
+  //////////////////////
+
+    io::Serializer FragmentMutateSmilesReact::GetSerializer() const
+    {
+      io::Serializer parameters( FragmentMutateInterface::GetSerializer());
+      parameters.SetClassDescription
+      (
+        "Appends a classic medicinal chemistry functional group to the current molecule"
+      );
+
+      parameters.AddInitializer
+      (
+        "medchem_library",
+        "path to the medchem library",
+        io::Serialization::GetAgent( &m_MedChemFilename),
+        RotamerLibraryFile::GetRotamerFinder().FindFile( "") + "medchem_fragments/bcl_buildfrag_0.sdf.gz"
+      );
+
+      return parameters;
+    }
+
+    //! @brief Set the members of this property from the given LABEL
+    //! @param LABEL the label to parse
+    //! @param ERROR_STREAM the stream to write errors to
+    bool FragmentMutateSmilesReact::ReadInitializerSuccessHook( const util::ObjectDataLabel &LABEL, std::ostream &ERROR_STREAM)
+    {
+      // static initialization check
+      if( command::CommandState::IsInStaticInitialization())
+      {
+        return true;
+      }
+
+      // call RISH function of the base class
+      if( !FragmentMutateInterface::ReadInitializerSuccessHook( LABEL, ERROR_STREAM))
+      {
+        return false;
+      }
+
+      // read in ring library filename
+      if( m_MedChemFilename.size())
+      {
+        s_Mutex.Lock();
+        io::IFStream input;
+        io::File::MustOpenIFStream( input, m_MedChemFilename);
+        FragmentEnsemble medchem_groups;
+        medchem_groups.ReadMoreFromMdl( input);
+        m_FragmentPool = util::CloneToShPtr( medchem_groups);
+        io::File::CloseClearFStream( input);
+        s_Mutex.Unlock();
+      }
+
+      // done
+      return true;
+    }
+
+  //////////////////////
+  // input and output //
+  //////////////////////
+
+    //! @brief read from std::istream
+    //! @param ISTREAM input stream
+    //! @return istream which was read from
+    std::istream &FragmentMutateSmilesReact::Read( std::istream &ISTREAM)
+    {
+      io::Serialize::Read( m_FragmentPool, ISTREAM);
+      return ISTREAM;
+    }
+
+    //! @brief write to std::ostream
+    //! @param OSTREAM output stream
+    //! @param INDENT number of indentations
+    //! @return ostream which was written to
+    std::ostream &FragmentMutateSmilesReact::Write( std::ostream &OSTREAM, const size_t INDENT) const
+    {
+      io::Serialize::Write( m_FragmentPool, OSTREAM, INDENT) << '\n';
+      return OSTREAM;
+    }
+
+  } // namespace chemistry
+} // namespace bcl
